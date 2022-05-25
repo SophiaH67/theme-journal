07ba85db685d3f6aeedaca55e7149ef0fe650748
marnixah <marnix@hagehoeve.com>
2022-05-25 23:39:07 +0200

    fix: persist user Id with state

--- components/goals/Goals.tsx
+++ components/goals/Goals.tsx
@@ -1,10 +1,17 @@
-import { collection, orderBy, query, where } from "firebase/firestore";
+import {
+  collection,
+  orderBy,
+  query,
+  QuerySnapshot,
+  where,
+} from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { auth, firestore } from "../../lib/app";
 import { converter, Goal } from "../../lib/goals";
 import GoalTableEntry from "./GoalTableEntry";
 import { Dayjs } from "dayjs";
+import { useState } from "react";
 
 export default function Goals({
   startDate,
@@ -14,23 +21,27 @@ export default function Goals({
   endDate: Dayjs;
 }) {
   const [user] = useAuthState(auth);
+  const [userId, setUserId] = useState(
+    typeof window !== "undefined" ? localStorage.getItem("userId") || "" : ""
+  );
+
+  if (user) {
+    if (user.uid !== userId) {
+      localStorage.setItem("userId", user.uid);
+      setUserId(user.uid);
+    }
+  }
+
   const goalsCollection = collection(firestore, "goals").withConverter(
     converter
   );
 
-  // Only query for documents where doc.\owner === user.uid
-
-  const [goals, loading, error] = useCollection<Goal>(
-    query(goalsCollection, where("owner", "==", user?.uid || "abc123"))
+  const [fGoals, loading, error] = useCollection<Goal>(
+    query(goalsCollection, where("owner", "==", userId))
   );
 
-  if (!user) {
-    return <div>You must be logged in to view this page.</div>;
-  }
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const mockGoals = { docs: [] } as any as QuerySnapshot<Goal>;
+  const goals: QuerySnapshot<Goal> = loading ? mockGoals : fGoals || mockGoals;
 
   if (error) {
     return <div>Error: {JSON.stringify(error)} </div>;
@@ -45,38 +56,34 @@ export default function Goals({
 
   return (
     <>
-      {goals ? (
-        <table
-          className="max-w-full border-collapse text-sm md:text-3xl"
-          cellPadding="4rem"
-        >
-          <thead>
-            <tr className="border-b-2 border-gray-200">
-              <th>Title</th>
-              {Array.from(Array(daysDiff).keys()).map((day) => (
-                <th
-                  key={day}
-                  className="w-10 border-x border-gray-200 p-1 md:w-20"
-                >
-                  {startDate.add(day, "day").format("ddd")}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {sortedGoals?.map((goal) => (
-              <GoalTableEntry
-                key={goal.id}
-                goal={goal}
-                days={daysDiff}
-                startDate={startDate}
-              />
+      <table
+        className="max-w-full border-collapse text-sm md:text-3xl"
+        cellPadding="4rem"
+      >
+        <thead>
+          <tr className="border-b-2 border-gray-200">
+            <th>Title</th>
+            {Array.from(Array(daysDiff).keys()).map((day) => (
+              <th
+                key={day}
+                className="w-10 border-x border-gray-200 p-1 md:w-20"
+              >
+                {startDate.add(day, "day").format("ddd")}
+              </th>
             ))}
-          </tbody>
-        </table>
-      ) : (
-        <div>No goals</div>
-      )}
+          </tr>
+        </thead>
+        <tbody>
+          {sortedGoals?.map((goal) => (
+            <GoalTableEntry
+              key={goal.id}
+              goal={goal}
+              days={daysDiff}
+              startDate={startDate}
+            />
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
